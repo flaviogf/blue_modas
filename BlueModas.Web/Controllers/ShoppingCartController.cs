f275d76f316c8e69b56157b5fb341a5f79acbeac
flaviogf <flavio.fernandes6@gmail.com>
2020-06-06 18:17:17 -0300

    feat: getting current order at shopping cart

--- BlueModas.Web/Controllers/ShoppingCartController.cs
+++ BlueModas.Web/Controllers/ShoppingCartController.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Threading.Tasks;
+using BlueModas.Web.Services;
+using BlueModas.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlueModas.Web.Controllers
@@ -5,11 +9,34 @@ namespace BlueModas.Web.Controllers
     [Route("[controller]")]
     public class ShoppingCartController : Controller
     {
+        private readonly IOrderService _orderService;
+
+        public ShoppingCartController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
         [HttpGet]
         [Route("")]
-        public IActionResult Show()
+        public async Task<IActionResult> Show()
         {
-            return View();
+            if (HttpContext.Session.TryGetValue("@order-number", out var value))
+            {
+                var orderNumber = new Guid(value);
+
+                var result = await _orderService.FindByNumber(orderNumber);
+
+                if (result.IsFailure)
+                {
+                    TempData["Failure"] = "Não foi possível carregar o carrinho";
+
+                    return View(new OrderShowViewModel());
+                }
+
+                return View(result.Value);
+            }
+
+            return View(new OrderShowViewModel());
         }
     }
 }
