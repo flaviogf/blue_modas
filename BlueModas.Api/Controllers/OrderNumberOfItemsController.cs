8b1cdd10d331c614e513cf7dcc3f0d1ea3d236b4
flaviogf <flavio.fernandes6@gmail.com>
2020-06-06 18:14:34 -0300

    feat: adding order number of items controller

---
+++ BlueModas.Api/Controllers/OrderNumberOfItemsController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using BlueModas.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace BlueModas.Api.Controllers
+{
+    [ApiController]
+    [Route("Order/{number}/NumberOfItems")]
+    public class OrderNumberOfItemsController : ControllerBase
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderNumberOfItemsController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [SwaggerOperation(Tags = new[] { "Order" })]
+        public ActionResult<int> Show([FromRoute] Guid number)
+        {
+            var maybeOrder = _orderRepository.FindByNumber(number);
+
+            if (!maybeOrder.HasValue)
+            {
+                return NotFound();
+            }
+
+            var order = maybeOrder.Value;
+
+            var sum = order.Items.Sum(it => it.Quantity);
+
+            return Ok(sum);
+        }
+    }
+}
