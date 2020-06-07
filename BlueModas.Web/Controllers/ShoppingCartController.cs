b9f7b41569ea49c381700bf34bb54666e9565222
flaviogf <flavio.fernandes6@gmail.com>
2020-06-07 06:36:59 -0300

    refactor: adding early returning at shopping cart controller

--- BlueModas.Web/Controllers/ShoppingCartController.cs
+++ BlueModas.Web/Controllers/ShoppingCartController.cs
@@ -20,23 +20,23 @@ namespace BlueModas.Web.Controllers
         [Route("")]
         public async Task<IActionResult> Show()
         {
-            if (HttpContext.Session.TryGetValue("@order-number", out var value))
+            if (!HttpContext.Session.TryGetValue("@order-number", out var value))
             {
-                var orderNumber = new Guid(value);
-
-                var result = await _orderService.FindByNumber(orderNumber);
-
-                if (result.IsFailure)
-                {
-                    TempData["Failure"] = "Não foi possível carregar o carrinho";
-
-                    return View(new OrderShowViewModel());
-                }
-
-                return View(result.Value);
+                return View(new OrderShowViewModel());
             }
 
-            return View(new OrderShowViewModel());
+            var orderNumber = new Guid(value);
+
+            var result = await _orderService.FindByNumber(orderNumber);
+
+            if (result.IsFailure)
+            {
+                TempData["Failure"] = "Não foi possível carregar o carrinho";
+
+                return View(new OrderShowViewModel());
+            }
+
+            return View(result.Value);
         }
     }
 }
