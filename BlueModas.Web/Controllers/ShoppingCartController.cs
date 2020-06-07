678342250dea56738ceaf43b19f30bd0e0bbe65a
flaviogf <flavio.fernandes6@gmail.com>
2020-06-07 07:21:26 -0300

    feat: changign shopping cart route

--- BlueModas.Web/Controllers/ShoppingCartController.cs
+++ BlueModas.Web/Controllers/ShoppingCartController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BlueModas.Web.Controllers
 {
-    [Route("[controller]")]
+    [Route("MeuCarrinho")]
     public class ShoppingCartController : Controller
     {
         private readonly IOrderService _orderService;
