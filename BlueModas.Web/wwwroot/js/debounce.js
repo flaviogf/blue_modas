a6891f47b4444f3f8503258853c20105e052c686
flaviogf <flavio.fernandes6@gmail.com>
2020-06-07 17:44:09 -0300

    feat: adding debounce function

---
+++ BlueModas.Web/wwwroot/js/debounce.js
@@ -0,0 +1,11 @@
+function debounce(fn, delay) {
+  let timeout = null;
+
+  return function () {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+
+    timeout = setTimeout(() => fn.apply(this, arguments), delay);
+  };
+}
