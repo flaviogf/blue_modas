ae3c52cb977508f2a1e80e1b0c9b8c1d48096cf4
flaviogf <flavio.fernandes6@gmail.com>
2020-06-07 09:19:57 -0300

    refactor: removing unused property at order item

--- BlueModas.Web/ViewModels/OrderItemStoreViewModel.cs
+++ BlueModas.Web/ViewModels/OrderItemStoreViewModel.cs
@@ -1,11 +1,7 @@
-using System;
-
 namespace BlueModas.Web.ViewModels
 {
     public class OrderItemStoreViewModel
     {
-        public Guid OrderNumber { get; set; }
-
         public int ProductId { get; set; }
     }
 }
